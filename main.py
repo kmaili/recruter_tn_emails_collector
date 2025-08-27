96f690614d286a4f893c1a2dac57c5626148b7b7
kmaili <kmailiaymen10@gmail.com>
2025-08-27 19:47:51 +0100

    add params

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from filters.contains_keywords import ContainsKeywordsFilter
 from scraper import EmailScraper
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Exemple de params")
+parser.add_argument("--query", type=str, help="specify the search query")
+parser.add_argument("--pages", type=int, help="number of pages to scrape")
+parser.add_argument("--output", type=str, help="output file name")
+
 
 if __name__ == "__main__":
-    filter = ContainsKeywordsFilter(["web", "django", "angular"])
-    scraper = EmailScraper(headless=True, keywords="angular", filters=[filter])
-    scraper.extract_data()
+    args = parser.parse_args()
+    if not args.query:
+        print("Please provide a search query using --query")
+        exit(1)
+    if not args.output:
+        print("Please provide an output file name using --output")
+        exit(1)
+    query = args.query
+    output_file = args.output
+    pages = args.pages if args.pages else 10
+    # filter = ContainsKeywordsFilter(["web", "django", "angular"])
+    # scraper = EmailScraper(headless=True, keywords=query, filters=[filter])
+
+
+    scraper = EmailScraper(headless=True, keywords=query)
+    scraper.extract_data(pages=pages, output_file=output_file+".csv")
     scraper.quit()
--- scraper.py
+++ scraper.py
@@ -1,6 +1,14 @@
 from driver import Driver
 from mixins.from_recruter_tn import FromRecruterTnMixin
+import logging
 
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 class EmailScraper(Driver, FromRecruterTnMixin):
     def __init__(self, headless=False, keywords="angular", filters=None):
@@ -9,31 +17,32 @@ class EmailScraper(Driver, FromRecruterTnMixin):
         super().__init__(headless=headless, keywords=keywords)
 
     
-    def extract_data(self, pages=10):
-        self._empty_file("results.csv")
+    def extract_data(self, pages, output_file):
+        self._empty_file(output_file)
         for data in self._extract_data(pages=pages):
             valid = True
             for filter in self.filters:
                 if not filter.check(data["job_description"]):
                     valid = False
+                    logger.info(f"Filtered out job due to filter {filter.__class__.__name__}")
                     break
             if not valid:
                 continue
             data.pop("job_description")
-            self.append_to_file("results.csv", data)
+            self.append_to_file(output_file, data)
+            logger.info(f"stored email: {data['email']}")
 
     # append to csv file
     def append_to_file(self, filename, data):
         import csv
         import os
-
         file_exists = os.path.isfile(filename)
         with open(filename, mode='a', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=data.keys())
             if not file_exists:
                 writer.writeheader()
             writer.writerow(data)
-    
+        
     def _empty_file(self, filename):
         import os
         if os.path.isfile(filename):
